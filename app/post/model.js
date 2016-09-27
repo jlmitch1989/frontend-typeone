6d78455f7a9c6fc3980fc9f94a3a97dc85ebc8f3
jlmitch1989 <joshuamitchell@Joshuas-MacBook-Pro.local>
2016-09-27 09:17:06 -0400

    Show post titles
    Successfully displays post titles from the back end.

---
+++ app/post/model.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  // content: DS.attr('string'),
+  // userId: DS.attr('string'),
+  // added this to add relationship
+  // users: DS.belongsTo('user'),
+});
