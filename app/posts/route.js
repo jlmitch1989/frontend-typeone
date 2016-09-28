c77729a366f71c43e973606df3b3d9b45ed26613
jlmitch1989 <joshuamitchell@Joshuas-MacBook-Pro-2.local>
2016-09-28 15:53:31 -0400

    Fix view transition bug
    When clicking between `forum` and `my thread` buttons,
    query params kept sticking because I forgot to make some changes.

--- app/posts/route.js
+++ app/posts/route.js
@@ -1,13 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    user: {
+      refreshModel: true
+    }
+  },
+
   model(params) {
     return this.get('store').query('post', params);
   }
-
-  // model () {
-  //   return this.get('store').findAll('post');
-  // },
 
   // actions: {
   //   deletePosts (post) {
