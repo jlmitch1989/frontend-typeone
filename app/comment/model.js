a2f5ae3241039e865c13e7786ad2be0a16cc13ae
jlmitch1989 <joshuamitchell@Joshuas-MacBook-Pro-2.local>
2016-09-30 11:56:40 -0400

    Fix current user button bug
    Add editable boolean to show only current user buttons.

--- app/comment/model.js
+++ app/comment/model.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   body: DS.attr('string'),
+  editable: DS.attr('boolean'),
   post: DS.belongsTo('post')
 });
--- app/post/model.js
+++ app/post/model.js
@@ -3,7 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   content: DS.attr('string'),
-  // userId: DS.attr('string'),
+  editable: DS.attr('boolean'),
+  user_id: DS.attr('number'),
   // added this to add relationship
   comments: DS.hasMany('comment')
 });
